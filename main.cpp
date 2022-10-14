001ae7b75551ef0a4086f0d76afe48dc1dc4797a
151804-PO12 <115485281+151804-PO12@users.noreply.github.com>
2022-10-14 15:21:45 +0200

    Pierwszy commit
    Póki co kod deklaruje kilka klas i nic więcej.

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Cat
+{
+    public:
+        string name;
+        int age;
+        float weigthkg;
+        string furcolour;
+        string eyecolour;
+};
+
+class Person
+{
+    public:
+        string name;
+        int age;
+        float money;
+        int heightcm;
+};
+
+class Box
+{
+    public:
+        int capacity;
+        string contents;
+};
+
+int main()
+{
+    return 0;
+}
