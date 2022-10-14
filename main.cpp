729333ec89e902c248e8868957f2c93aa05c19da
151804-PO12 <115485281+151804-PO12@users.noreply.github.com>
2022-10-14 15:34:59 +0200

    Update main.cpp
    Teraz program tworzy kilka przykładowych obiektów i wypisuje niektóre właściwości.
    Zmieniono nazwy niektórych zmiennych klas: Dodano więcej końcówek z jednostkami do nazw oraz końcówki zaczynają się dużą literą.

--- main.cpp
+++ main.cpp
@@ -6,29 +6,59 @@ class Cat
 {
     public:
         string name;
-        int age;
-        float weigthkg;
-        string furcolour;
-        string eyecolour;
+        int ageYears;
+        float weigthKg;
+        string furColour;
+        string eyeColour;
 };
 
 class Person
 {
     public:
         string name;
-        int age;
-        float money;
-        int heightcm;
+        int ageYears;
+        float moneyPln;
+        int heightCm;
 };
 
 class Box
 {
     public:
-        int capacity;
+        int capacityLitres;
         string contents;
 };
 
 int main()
 {
+    Cat Sonia;
+    Sonia.name = "Sonia";
+    Sonia.ageYears = 3;
+    Sonia.furColour = "white";
+    Sonia.eyeColour = "green";
+
+    cout << Sonia.name << " is " << Sonia.ageYears << " years old." << endl;
+
+    Cat Milka;
+    Milka.name = "Milka";
+    Milka.ageYears = 1;
+    Milka.furColour = "black";
+    Milka.eyeColour = "green";
+
+    Person Catowner;
+    Catowner.name = "John";
+    Catowner.ageYears = 20;
+    Catowner.heightCm = 19*10;
+
+    cout << Catowner.name << "'s height is " << Catowner.heightCm << "cm." << endl;
+
+    Box Lunchbox;
+    Lunchbox.capacityLitres = 2;
+    Lunchbox.contents = "sandwich";
+
+    cout << "There's " << Lunchbox.contents << " inside this box." << endl;
+
+    Box Bin;
+    Bin.capacityLitres = 120;
+    Bin.contents = "laundry";
     return 0;
 }
