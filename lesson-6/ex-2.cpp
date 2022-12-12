607214af1b38fa31833654df8ac3b9ee3bb9b9e6
151804-PO12 <115485281+151804-PO12@users.noreply.github.com>
2022-12-12 17:46:14 +0100

    Moved exception handling to main

--- lesson-6/ex-2.cpp
+++ lesson-6/ex-2.cpp
@@ -12,18 +12,21 @@ class Logarithm : public Number {
  private:
   double base, argument;
  public:
-  double doubleValue() { return log(this->argument) / log(this->base);}
-  Logarithm(double inputBase, double inputArgument) {
+  double doubleValue()
+  {
     try
     {
-        if(inputBase == 1)
-            throw "Base cannot be equal to 1.";
-        if(inputArgument <= 0)
-            throw "Argument cannot be a non-positive number.";
-    }catch (char const* error)
+        if(this->base == 1)
+            throw exception();
+        else if(this->argument <= 0)
+            throw exception();
+    }catch(exception)
     {
-        cout << "Incorrect argument: " << error << endl;
+        throw exception();
     }
+    return log(this->argument) / log(this->base);
+  }
+  Logarithm(double inputBase, double inputArgument) {
     this->base = inputBase;
     this->argument = inputArgument;
   }
@@ -31,7 +34,14 @@ class Logarithm : public Number {
 
 int main()
 {
-    Logarithm(2 ,0);
+    try
+    {
+        Logarithm(2 ,0).doubleValue();
+    }
+    catch (exception)
+    {
+        cout << "Incorrect argument" << endl;
+    }
 
     return 0;
 }
